4d9ba762129feb5d8194c763fa3b343bda54c001
hongwilliam <willhong20@gmail.com>
2017-12-06 20:27:19 -0500

    Added -v and -r, but -v doesn't work satisfactory, refer to comments

--- semaphore.c
+++ semaphore.c
@@ -5,20 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define KEY 1287
+#include <errno.h>
+#define KEY 125182
 //note while testing, once you compile code, semaphore will already exist for defined KEY
 
 
 //we need to take arguments to main in order to handle command line args
 //shoutout to gbdirect.co.uk
 int main(int argc, char *argv[]){
-
-  /* using fgets below got me nowhere
-  char input[1024];
-  printf("Enter input here: ");
-  fgets(input, 1024, stdin);
-  *strchr(input, '\n') = 0; //insert terminating NULL
-  */
+  //semaphore descriptor sd will be used to input into semctl
+  //we want to create 1 semaphore
+  int sd;
+  int n; //refer to -c N code
 
   //-c N
   //create a semaphore and set value to N
@@ -28,26 +26,38 @@ int main(int argc, char *argv[]){
   if (argc == 3){
     //if the first part of args is -c
     if (strncmp(argv[1], "-c", 2) == 0){
-      //semaphore descriptor sd will be used to input into semctl
       //we want to create 1 semaphore
-      int sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
-
+      sd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);
       //if descriptor is 0, then we know it already exists
-      if (sd > 0){
-        int n;
+      if (sd >= 0){
         sscanf(argv[2], "%d", &n); //scan the string for the inputted n value
         printf("n value is %s \n", argv[2]);
         printf("semaphore created: %d \n", sd);
 
         //we're setting value of semaphore
         semctl(sd, 1, SETVAL, n);
-        printf("value set: %d \n", semctl(sd, 1, GETVAL));
-      }
+        printf("value set: %d \n", semctl(sd, 1, GETVAL)); }
       else{
-        //if semaphore has value of 0, then it is unavailable
-          printf("semaphore already exists \n"); }
+            printf("semaphore already exists \n"); }
+      }
+    }
+
+    if (argc == 2){
+        //-v
+        //view the CURRENT value of the semaphore
+        //i haven't figured this out correctly
+        if (strncmp(argv[1], "-v", 2) == 0){
+          //include no flags, we are not creating a new semaphore
+          sd = semget(KEY, 1, 0644);
+          printf("semaphore value: %d \n", semctl(sd, 1, GETVAL)); }
+
+        //-r
+        //remove the semaphore
+        if (strncmp(argv[1], "-r", 2) == 0){
+          sd = semget(KEY, 1, 0644);
+          //setting value of semaphore to 0 makes it unavaiable
+          printf("semaphore removed: %d \n", semctl(sd, 1, SETVAL, 0)); }
 
     }
 
-  }
 }
